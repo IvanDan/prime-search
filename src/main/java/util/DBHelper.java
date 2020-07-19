710ac2bfd576ad3d89a3820fe799cff0ce770f1c
Ivan <ivandanza@gmail.com>
2020-07-19 12:51:44 +0200

    Update DBHelper.java

--- src/main/java/util/DBHelper.java
+++ src/main/java/util/DBHelper.java
@@ -1,4 +1,24 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBHelper {
+  private final String url = "jdbc:postgresql://localhost/myDatabase";
+  private final String user = "postgres";
+  private static DBHelper dbHelper = null;
+  private Connection con;
+
+  private DBHelper(String password) throws SQLException {
+    con = DriverManager.getConnection(url, user, password);
+  }
+
+  public static DBHelper createDBHelper(String password) throws SQLException {
+    if (dbHelper == null) {
+      dbHelper = new DBHelper(password);
+      return dbHelper;
+    } else
+      return dbHelper;
+  }
 }
