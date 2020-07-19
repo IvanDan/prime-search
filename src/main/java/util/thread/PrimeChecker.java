15b83e741bacd3260e83c7464e6e9a72002dcf2e
Ivan <ivandanza@gmail.com>
2020-07-19 18:27:41 +0200

    Update PrimeChecker.java

--- src/main/java/util/thread/PrimeChecker.java
+++ src/main/java/util/thread/PrimeChecker.java
@@ -1,5 +1,39 @@
 package util.thread;
 
-public class PrimeChecker extends Thread {
+import util.database.DBHelperImpl;
+import util.number.NumberDTO;
+import util.prime.PrimeCheck;
 
+import java.util.concurrent.Semaphore;
+
+public class PrimeChecker extends Thread {
+  private DBHelperImpl dbHelper;
+  private Semaphore sem;
+
+  public PrimeChecker(DBHelperImpl dbHelper, Semaphore sem) {
+    this.dbHelper = dbHelper;
+    this.sem = sem;
+    start();
+  }
+
+  @Override
+  public void run() {
+    NumberDTO numberToCheck;
+    do {
+      try {
+        sem.acquire();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      numberToCheck = dbHelper.searchNumberForCheck();
+      if (numberToCheck != null) {
+        numberToCheck.setIsPrime(-2);
+        sem.release();
+
+        numberToCheck.setIsPrime(PrimeCheck.check(numberToCheck.getNum()));
+        dbHelper.updateNumber(numberToCheck);
+      } else
+        sem.release();
+    } while (numberToCheck != null);
+  }
 }
