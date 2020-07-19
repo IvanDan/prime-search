2408d88bc33b27596e6be00b3b24ad82fd306de5
Ivan <ivandanza@gmail.com>
2020-07-19 14:31:38 +0200

    added isPrime attribute

--- src/main/java/util/NumberDTO.java
+++ src/main/java/util/NumberDTO.java
@@ -2,6 +2,20 @@ package util;
 
 public class NumberDTO {
   private int num;
+  private boolean isPrime;
+
+  public NumberDTO(int num, boolean isPrime) {
+    this.num = num;
+    this.isPrime = isPrime;
+  }
+
+  public boolean isPrime() {
+    return isPrime;
+  }
+
+  public void setPrime(boolean prime) {
+    isPrime = prime;
+  }
 
   public NumberDTO(int num) {
     this.num = num;
