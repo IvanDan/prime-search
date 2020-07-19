610301685ebd7221d24342e84f549a0bca29092f
Ivan <ivandanza@gmail.com>
2020-07-19 19:02:21 +0200

    added javadoc

--- src/main/java/util/database/DBHelper.java
+++ src/main/java/util/database/DBHelper.java
@@ -7,18 +7,31 @@ public interface DBHelper {
   String USER = "postgres";
 
   /**
-   * Questo metodo aggiunge un numero al database
-   *
-   * @param number il numero da aggiungere
-   * @return <code>true</code> se l'inserimento va a buon fine, <code>false</code> altrimenti
+   * @param number the number to add
+   * @return <code>true</code> true if added, <code>false</code> otherwise
    */
   boolean addNumber(int number);
 
+  /**
+   * @param number the number to be update
+   * @return <code>true</code> if successful, <code>false</code> otherwise
+   */
   boolean updateNumber(NumberDTO number);
 
+  /**
+   * @param number the number to search
+   * @return the object of the number searched, null otherwise
+   */
   NumberDTO getNumber(int number);
 
+  /**
+   * @param number the number of which you want to check whether it is prime or not
+   * @return 1 if it is prime, 0 if it is not, and -1 if it has never been checked
+   */
   int isPrime(int number);
 
+  /**
+   * @return the row of the smallest number that has not yet been checked, null otherwise
+   */
   NumberDTO searchNumberForCheck();
 }
