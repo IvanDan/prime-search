3187d4f509ab630cc3104d704eb8abd4846c0c87
Ivan <ivandanza@gmail.com>
2020-07-20 19:08:39 +0200

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -11,7 +11,8 @@ public class Main {
   public static void main(String[] args) throws SQLException, InterruptedException {
     DBHelperImpl dbh = DBHelperImpl.createDBHelper("postgres");
     Semaphore sem = new Semaphore(1);
-    PrimeChecker[] checkers = new PrimeChecker[1000];
+    PrimeChecker[] checkers = new PrimeChecker[50];
+
 
     for (int i = 0; i < checkers.length; i++)
       checkers[i] = new PrimeChecker(dbh, sem);
