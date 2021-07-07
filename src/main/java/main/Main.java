8d25b16c3d09a8b57c619177aae49abcfad2cf95
IvanDan <ivandanza@gmail.com>
2021-07-07 10:25:03 +0200

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -11,11 +11,17 @@ public class Main {
   public static void main(String[] args) throws SQLException, InterruptedException {
     DBHelperImpl dbh = DBHelperImpl.createDBHelper("postgres");
     Semaphore sem = new Semaphore(1);
-    PrimeChecker[] checkers = new PrimeChecker[50];
+    PrimeChecker[] checkers = new PrimeChecker[10];
+
+//    Eratostene eratostene = new Eratostene();
+//    ArrayList list = eratostene.findPrime(999999);
+//    System.out.println(list);
 
     // check if there are no numbers in busy state (-2)
     dbh.changeStatus(-1);
 
+//    for (int i = 100001; i < 200000; i++)
+//      dbh.addNumber(200000);
     for (int i = 0; i < checkers.length; i++)
       checkers[i] = new PrimeChecker(dbh, sem);
     for (int i = 0; i < checkers.length; i++)
